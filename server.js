b379d086bb45c91856cfe148317cd609d19d7d68
grimes amani <grimesamani98@gmail.com>
2026-07-13 18:59:03 +0100

    تشييد قلعة الحماية السباعية الفولاذية للمنصة: ترويسات الخادم، مصيدة المطورين، اعتراض القص، والخاتم المائي

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ const SECURITY_HEADERS = {
   'X-Content-Type-Options': 'nosniff',
   'X-Frame-Options': 'SAMEORIGIN',
   'X-XSS-Protection': '1; mode=block',
+  'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload',
   'Referrer-Policy': 'strict-origin-when-cross-origin',
   'Permissions-Policy': 'camera=(), microphone=(), geolocation=()',
   'Access-Control-Allow-Origin': '*'
